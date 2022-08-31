5f60d3673047cd446a32a6f6b840edeac5d72960
felipe911 <felipe20911@gmail.com>
2022-08-31 18:28:48 -0300

    Geração do projeto, conexão com Mysql e classe model Aeronave

---
+++ src/main/java/com/airplanes/sonda/model/Aeronave.java
@@ -0,0 +1,62 @@
+package com.airplanes.sonda.model;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Entity
+@Table(name = "aeronave")
+public class Aeronave {
+	
+	@Id
+	@JsonProperty
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@SequenceGenerator(name = "service_sequence", sequenceName = "service_sequence", allocationSize=1)
+	private Long id;
+
+	@NotNull
+	@JsonProperty
+	@Column(name = "nome")
+	private String nome;
+
+	@NotNull
+	@JsonProperty
+	@Column(name = "marca")
+	private String marca;
+
+	@NotNull
+	@JsonProperty
+	@Column(name = "ano")
+	private Integer ano;
+
+	@NotNull
+	@JsonProperty
+	@Column(name = "descricao")
+	private String descricao;
+
+	@NotNull
+	@JsonProperty
+	@Column(name = "vendido")
+	private Boolean vendido;
+
+	@NotNull
+	@JsonProperty
+	@Column(name = "created")
+	private Date created;
+
+	@NotNull
+	@JsonProperty
+	@Column(name = "updated")
+	private Date updated;
+
+}
